ff650f25ef32d2990093fc3186691bec2b8ffe88
Sleepy <idhammakbar@gmail.com>
2024-07-14 15:36:34 +0700

    [build] admin panel (section news masih error)

---
+++ routes/backpack/custom.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// --------------------------
+// Custom Backpack Routes
+// --------------------------
+// This route file is loaded automatically by Backpack\Base.
+// Routes you generate using Backpack\Generators will be placed here.
+
